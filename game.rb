186eae90aa4459ff73f4dfec7d7ff691c19eabe8
Felipe Arreola <arreolaf@oregonstate.edu>
2020-12-27 18:25:00 -0800

    checkpoint #2-'game.rb'

--- game.rb
+++ game.rb
@@ -11,10 +11,43 @@ class Game
   # Instance methods
   def initialize(:number_of_dice 2, :number_of_tiles 9)
     @dice = Array.new(:number_of_dice) {|num_of_dice| Dice.new(num_of_dice+1) }    
-    @tiles = Array.new(:number_of_tiles) {|num_of_tiles| Dice.new(num_of_tiles+1) }
+    @tiles = Array.new(:number_of_tiles) {|num_of_tiles| Tiles.new(num_of_tiles+1) }
     @gameRunning = true
-    
+    puts "Let's play SHUT THE BOX!\n"
+
+    # --- compact to a function: turn>?
+    # print- table status
+
+    roll_dice
+
+    ## print dice 
   end  
+
+  public
+  # Roll each die in array "Dice"
+  def roll_dice
+    @dice.each do |die| 
+      # reset roll value of die
+      die.reset
+      # roll and assign value
+      die.roll
+    end
+  end
+
+  def turn
+    puts "------------------\nRolling dice..."
+    roll_dice
+    ## print dice 
+
+  end
+
+  def print_dice_rolls
+
+  end
+
+  def gameRunning?
+    @gameRunning
+  end
 
 end
 

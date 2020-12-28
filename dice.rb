d38780949481e624a3ed8a6bf4bb6f0083fc981c
Felipe Arreola <arreolaf@oregonstate.edu>
2020-12-27 16:25:05 -0800

    added methods, attr readers

--- dice.rb
+++ dice.rb
@@ -1,5 +1,27 @@
 class Dice
 
-  #TODO: define dice
+  # Instance variables
+  @numberID     # integer value ID
+  @rollValue    # roll value (1-6)
+  
+  # Instance methods
+  def initialize(number)
+    @numberID = number
+    @rollValue = 0 
+  end
+
+  protected
+  attr_reader :numberID, :rollValue
+
+  public
+  # reset the roll value
+  def reset
+    @rollValue = 0
+  end
+
+  # roll the die and assign value
+  def roll
+    @rollValue = rand 1..6
+  end  
 
 end

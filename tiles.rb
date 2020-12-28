640d40d467ce7b867566519ef580e0a7d91517b4
Felipe Arreola <arreolaf@oregonstate.edu>
2020-12-27 16:03:22 -0800

    added methods, attr_readers

--- tiles.rb
+++ tiles.rb
@@ -1,6 +1,36 @@
-class Tile
+class Tiles
 
-  #TODO: define tiles
+  # Instance variables
+  @numberID     # integer value ID
+  @statusOpen   # Boolean: True: Open; False: Closed
+  
+  # Instance methods
+  def initialize(number)
+    @numberID = number
+    @statusOpen = true
+  end
+  
+  protected
+  attr_reader :numberID, :statusOpen
 
+  # Opens tile
+  def open
+    @statusOpen = true
+  end
+
+  # Closes tile
+  def close
+    @statusOpen = false
+  end
+
+  # returns status of tile
+  def open?
+    @statusOpen
+  end
+  
+  # returns status of tile
+  def close?
+    !@statusOpen
+  end
 
 end
